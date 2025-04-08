901f2100a81fc4089f1b1e15fcc43b944a1155c6
aman <ramgoswami3@gmail.com>
2025-04-08 22:41:09 +0530

    please hoja

--- test_app.py
+++ test_app.py
@@ -15,5 +15,5 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = 'wow ci-cd pipeline done now check'
+    expected_text = 'wow ci-cd pipeline done now check yaya'
     assert expected_text.encode() in response.data
